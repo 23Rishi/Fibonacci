c2ed56bcd8456bcd9bf713085a29d4d03ea4ea08
23Rishi <66222209+23Rishi@users.noreply.github.com>
2020-06-01 13:31:34 +0530

    To print the Positive numbers

---
+++ To Print Positive NO.py
@@ -0,0 +1,26 @@
+# Python program to print positive Numbers in a List
+#************************** For list1s ****************************
+
+# list of numbers 
+list1 = [12, -7, 5,64, -14] 
+
+# iterating each number in list 
+for num in list1: 
+	
+# checking condition 
+    if num >= 0: 
+        print(num, end = " ")
+
+
+#************************** For list2 ****************************
+
+
+# list of numbers 
+list2 = [12, 14, -95, 3 ] 
+
+# iterating each number in list 
+for num in list2: 
+	
+# checking condition 
+    if num >= 0: 
+        print(num, end = " ") 
